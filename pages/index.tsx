e53ea8135e5c0b295738e3dae926a0aa21c13ec2
hyolim lim <hyolimlim.dev@gmail.com>
2023-02-19 09:35:18 +0900

    feat: Add new images and fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import Nav from "@/components/Nav";
+import Slide from "@/components/Slide";
+import Section1 from "@/components/Section1";
+import Section2 from "@/components/Section2";
+import Section3 from "@/components/Section3";
+import Section4 from "@/components/Section4";
 
 type Props = {};
 
 const index = (props: Props) => {
-  return <div>index</div>;
+  return (
+    <div className="h-fit flex flex-col">
+      <Nav />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+    </div>
+  );
 };
 
 export default index;
