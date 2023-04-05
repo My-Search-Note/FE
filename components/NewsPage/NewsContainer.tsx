16641114cd2c84c83215ea75b218cf659b1eebf8
hyolim lim <hyolimlim.dev@gmail.com>
2023-04-05 12:45:41 +0900

    feat: Add NewsPage

--- components/NewsPage/NewsContainer.tsx
+++ components/NewsPage/NewsContainer.tsx
@@ -1,13 +1,42 @@
 import React from "react";
-import List from "../common/List";
+import NewsList from "./NewsList";
+import styles from "@/styles/searchStyle.module.scss";
 
-type Props = {};
+type Source = {
+  id: string | null;
+  name: string;
+};
 
-const NewsContainer = (props: Props) => {
+type Article = {
+  source: Source;
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
+};
+
+type ArticleListProps = {
+  articles: Article[];
+};
+
+const NewsContainer = ({ articles }: ArticleListProps) => {
   return (
     <div className="w-full h-full flex flex-col px-3">
-      <div className="h-full bg-white px-4 md:px-0 drop-shadow-sm lg:drop-shadow-xl flex flex-col justify-center items-center">
-        <List />
+      <div className="h-full bg-white px-4 md:px-0 drop-shadow-sm lg:drop-shadow-xl flex flex-col items-center">
+        <div className="w-full h-32 px-5 py-3">
+          <p className="text-2xl lg:text-4xl font-semibold">Top 10 headlines</p>
+          <p className="text-sm lg:text-lg lg:mt-2">
+            Headlines are updated every hour.
+          </p>
+        </div>
+        <div
+          className={`${styles["hide-scrollbar"]} h-[calc(100%_-_8rem)] overflow-y-scroll p-3 grid gap-y-8`}
+        >
+          <NewsList articles={articles} />
+        </div>
       </div>
     </div>
   );
